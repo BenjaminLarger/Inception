3dd7f41c840cb587827514c3d2a2ab1c2c0e275f
BenjaminLarger <benjaminlarger.bl@gmail.com>
2024-07-25 17:05:26 +0200

    mysql

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
 # Start the MySQL server in the background
-sudo mysqld_safe &
-
-# Wait for the MySQL server to start
-while ! mysqladmin ping --silent; do
-    sleep 1
-done
-
-# Set the root password using the environment variable
-mysql -u root <<-EOSQL
-    ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-    FLUSH PRIVILEGES;
-EOSQL
-
-# Keep the container running
-#tail -f /dev/null #delete
-#sudo mysqld_safe &
-#sudo mysql -u root -p
-
+service mysql start;
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+exec mysqld_safe
