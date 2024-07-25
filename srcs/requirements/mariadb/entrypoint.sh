a2561502e250a4236975319b9fa634996c47ed5b
BenjaminLarger <benjaminlarger.bl@gmail.com>
2024-07-25 13:00:30 +0200

    env variable and volume

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+# Start the MySQL server in the background
 sudo mysqld_safe &
-sudo mysql -u root -p
 
+# Wait for the MySQL server to start
+while ! mysqladmin ping --silent; do
+    sleep 1
+done
 
-#service mysql start
-#tail -f /dev/null
+# Set the root password using the environment variable
+mysql -u root <<-EOSQL
+    ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+    FLUSH PRIVILEGES;
+EOSQL
+
+# Keep the container running
+#tail -f /dev/null #delete
+#sudo mysqld_safe &
+#sudo mysql -u root -p
+
