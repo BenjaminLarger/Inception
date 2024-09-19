01d8e001947964e41a2f3fd52e80a0959041c58d
Your Name <you@example.com>
2024-09-19 02:59:35 -0400

    w

--- srcs/requirements/mariadb/tools/mariadb_init.sh
+++ srcs/requirements/mariadb/tools/mariadb_init.sh
@@ -8,4 +8,9 @@ sed -i "s/MDB_ROOT_PASS/${MYSQL_ROOT_PASSWORD}/g" /usr/local/bin/wordpress.sql
 chown -R mysql:mysql /var/lib/mysql
 chmod -R 755 /var/lib/mysql
 
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    mysql_install_db --user=mysql --basedir=/usr/ --ldata=/var/lib/mysql/
+fi
+
 mysqld --init-file=/usr/local/bin/wordpress.sql
